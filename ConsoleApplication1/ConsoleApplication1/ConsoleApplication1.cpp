e323f68f69464c92eafb1239ba6a99363ee6f638
Dungeongate <teeganduong@gmail.com>
2017-02-22 23:05:46 -0600

    Added project folder

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int x;
+	cout << "Ayy lmao" << endl;
+	cin >> x;
+	return 0;
+}
+
