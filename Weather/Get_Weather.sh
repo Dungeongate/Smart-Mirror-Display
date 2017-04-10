106963037576d9e65e9608caeefce61676236c2b
RyanFaulkenberry100 <ryan.faulkenberry@ttu.edu>
2017-04-10 12:42:28 -0500

    Added weather functionality

--- Weather/Get_Weather.sh
+++ Weather/Get_Weather.sh
@@ -2,12 +2,21 @@
 
 # When called, this script gets weather info from the weather utility and
 # writes relevant data to ./Weather.txt
+# Weather information must be searched
+# NOTE: The first time a location is entered, this script will run slow
 # TODO: Permissions?
 
-# Get user's location from Weather.txt
-location = $(head -n 1 ./Weather.txt)
+# If an argument is passed, make it the new location, else use Weather.txt
+if [ $0 != ""]; then
+  station=$0
+  sed -i '1s/.*/$station/' Weather.txt
+else
+  station=$(head -n 1 ./Weather.txt)
+fi
 
+# Gather weather info
+info=`weather -f $station`
+currentTemp=`info | egrep -o "Temperature: .{5}"`
 
-
-# Gather weather info and output relevant parts.
-info = `weather $location`
+# Update Weather.txt
+sed -i '2s/.*/$currentTemp/' Weather.txt
